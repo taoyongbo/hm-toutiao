d6b3ab2db611c0972fa0430104bf09b14d5d5b3c
magicstf <287908079@qq.com>
2019-08-02 08:22:41 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+
 import ElementUi from 'element-ui'
+
 import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@/router'
 Vue.config.productionTip = false
+
 Vue.use(ElementUi)
+
 new Vue({
-  render: h => h(App)
+
+  render: h => h(App),
+  router
+
 }).$mount('#app')
